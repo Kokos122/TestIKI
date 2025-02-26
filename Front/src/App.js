83f6d12f136a7818d2811522d4a6ef013c679734
Kokos122 <tumashov.05@bk.ru>
2025-02-26 17:35:17 +0700

    Initial commit

---
+++ Front/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white animate-pulse">
+        Tailwind работает и Гей! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
