7ba0ea8f49718f5811c34020f1c477173b303112
Kokos122 <tumashov.0005@gmail.com>
2025-05-29 06:25:52 +0700

    походу я справился4

--- Front/src/TestsPage.js
+++ Front/src/TestsPage.js
@@ -1,4 +1,4 @@
-import api from 'api.js/.';
+import api from './api.js';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
